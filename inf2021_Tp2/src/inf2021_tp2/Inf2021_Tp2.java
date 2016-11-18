c5098418ec63b116ed9e6963c75890941c33234b
info <info@DESKTOP-2UAC81J>
2016-11-18 04:39:36 -0500

    corrigé bug @ Phrase, needs refactoring

--- inf2021_Tp2/src/inf2021_tp2/Inf2021_Tp2.java
+++ inf2021_Tp2/src/inf2021_tp2/Inf2021_Tp2.java
@@ -1,5 +1,7 @@
 package inf2021_tp2;
 
+import java.util.List;
+
 /**
  *
  * @author Elias touil
@@ -7,11 +9,26 @@ package inf2021_tp2;
 public class Inf2021_Tp2 {
 
     public static void main(String[] args) {
-        Phrase maPhrase = new Phrase("Salut je suis bill et j'aime les plante mauves.");
+        Phrase maPhrase = new Phrase("Salut   je suis bill et j'aime les plantes mauves.");
+        Phrase maPhrase2 = new Phrase("Salut je mange    des plantes mauves.");
         System.out.println(maPhrase.arrayMots);
+        System.out.println(maPhrase2.arrayMots); 
+        System.out.println();
         
-        Sac monSac = new Sac(maPhrase,1);
+        Sac monSac = new Sac( maPhrase,-1 );
+        Sac monSac2 = new Sac( maPhrase2,-1);
+        
         System.out.println(monSac.toString());
+        System.out.println(monSac2.toString());
+        System.out.println();
+        
+        
+        List<Bigramme> intersection = monSac.intersection(monSac2);
+        for (Bigramme bi : intersection){
+            System.out.println(bi.base + " ... " + bi.complement);
+        }
+        
+        System.out.println(monSac.similarite(monSac2));
     }
     
 }
